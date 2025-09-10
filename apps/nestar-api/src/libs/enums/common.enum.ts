12c97c0314b5e61f6d06d837aea5ae3638495930
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-09-11 03:30:23 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,18 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = "Something went wrong!",
+  NO_DATA_FOUND = "No data is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is failed!",
+  REMOVE_FAILED = "Remove failed!",
+  UPLOAD_FAILED = "Upload failed!",
+
+  PROVIDER_ALLOWED_FORMAT = "Please provider jpg, jpeg or png images!",
+  TOKEN_CREATION_FAILED = "Token creation error!",
+  NO_MEMBER_NICK = "No member with that member nick!",
+  BLOCKED_USER = "You have been blocked, contact restaurant!",
+  WRONG_PASSWORD = "Wrong password, please try again!",
+  NOT_AUTHENTICATED = "You are not authenticated, Please login first!",
+  SELF_SUBSCRIPTION_DENIED = "Self subscription is denied",   
+}
+
+ 
