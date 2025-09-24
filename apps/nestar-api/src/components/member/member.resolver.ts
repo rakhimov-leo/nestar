7aaf99167a4f5a96c337f19a35f14beb21a8c6ab
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-09-24 14:58:55 +0900

    fix: modify updateMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,12 +9,13 @@ import type { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
-@Resolver()
+@Resolver() //Bu yerda MemberResolver degan sinf yaratildi.
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
-	@Mutation(() => Member)
+	@Mutation(() => Member) //@Mutation->Bu yerda MemberResolver degan sinf yaratildi.
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
 		console.log('Mutation: signup');
 		return this.memberService.signup(input);
@@ -24,15 +25,6 @@ export class MemberResolver {
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		console.log('Mutation: login');
 		return this.memberService.login(input);
-	}
-
-	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		//objectId uzi bulishi kerakedi
-		console.log('Mutation: updateMember');
-		console.log(memberId);
-		return this.memberService.updateMember();
 	}
 
 	@UseGuards(AuthGuard)
@@ -49,6 +41,17 @@ export class MemberResolver {
 	public async checkAuthRoles(@AuthMember() AuthMember: Member): Promise<string> {
 		console.log('Query: checkAuthRoles');
 		return `Hi ${AuthMember.memberNick}, you are ${AuthMember.memberType} (memberId: ${AuthMember._id})`;
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: updateMember');
+		delete input._id;
+		return this.memberService.updateMember(memberId, input);
 	}
 
 	@Query(() => String)
